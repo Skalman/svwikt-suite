c405f08b8579da2da5b6881777e128bf63cb2b73
Dodde <dodde.wikt@gmail.com>
2014-01-20 19:39:53 +0100

    test

--- SvwiktSuite/SvwiktSuite/Program.cs
+++ SvwiktSuite/SvwiktSuite/Program.cs
@@ -1,5 +1,8 @@
 using System;
 using Gtk;
+using System.IO;
+using System.Xml.Serialization;
+using System.Data.SQLite;
 
 namespace SvwiktSuite
 {
@@ -11,6 +14,20 @@ namespace SvwiktSuite
 			MainWindow win = new MainWindow ();
 			win.Show ();
 			Application.Run ();
+
+			string folder = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+			var db = new SQLiteConnection (System.IO.Path.Combine (folder, "notes.db"));
+			db.CreateTable<Note>();
+			var note = new Note { Message = "Test Note" };
+			db.Insert (note);
+			Console.WriteLine ("{0}: {1}", note.Id, note.Message);
+		}
+
+		public class Note
+		{
+			[PrimaryKey, AutoIncrement]
+			public int Id { get; set; }
+			public string Message { get; set; }
 		}
 	}
 }
