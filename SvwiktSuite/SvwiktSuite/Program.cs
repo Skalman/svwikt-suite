22651357c054bd433a6f6cc55449cfbeb3750567
Dan Wolff <dan.d.wolff@gmail.com>
2014-01-21 10:47:25 +0100

    Merge

--- SvwiktSuite/SvwiktSuite/Program.cs
+++ SvwiktSuite/SvwiktSuite/Program.cs
@@ -1,5 +1,9 @@
 using System;
 using Gtk;
+using System.IO;
+//using System.Xml.Serialization;
+using System.Data.SQLite;
+//using System.Data.SQLite.Generic;
 
 namespace SvwiktSuite
 {
@@ -7,10 +11,49 @@ namespace SvwiktSuite
 	{
 		public static void Main (string[] args)
 		{
-			Application.Init ();
-			MainWindow win = new MainWindow ();
-			win.Show ();
-			Application.Run ();
+			Application.Init();
+			MainWindow win = new MainWindow();
+			win.Show();
+			Application.Run();
+
+			//kör databasläsningstest
+			ReadFromDbTest();
+		}
+
+		/// <summary>
+		/// Testmetod som läser från databas.
+		/// </summary>
+		public static void ReadFromDbTest()
+		{
+			//skapa connection string
+			string folder = "..\\..\\data";
+			string connectionString = "URI=file:" + System.IO.Path.Combine (folder, "test.db");
+
+			//öppna databasen
+			SQLiteConnection db = new SQLiteConnection(connectionString);
+			db.Open();
+
+			//SQL query
+			SQLiteCommand cmd = db.CreateCommand();
+			cmd.CommandText = "SELECT pagename, tmpl_syntax FROM main";
+
+			//läs de första 100 raderna från databasen och skriv ut dem i konsollen
+			SQLiteDataReader reader =  cmd.ExecuteReader();
+			int count = 0;
+			while (reader.Read() && count < 100) {
+				string column1Data = reader.GetString(0);
+				string column2Data = reader.GetString(1);
+				Console.WriteLine(String.Format ("{0} : {1}", column1Data, column2Data));
+				count++;
+			}
+
+			//rensa upp
+			reader.Close();
+			reader = null;
+			cmd.Dispose();
+			cmd = null;
+			db.Close();
+			db = null;
 		}
 	}
 }
