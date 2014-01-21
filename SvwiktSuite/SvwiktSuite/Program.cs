f1b79d3a12811bd4d0fd8f862b760437425aeb94
Dan Wolff <dan.d.wolff@gmail.com>
2014-01-21 12:12:59 +0100

    Sqlite for Linux with Mono.Data.Sqlite

--- SvwiktSuite/SvwiktSuite/Program.cs
+++ SvwiktSuite/SvwiktSuite/Program.cs
@@ -2,8 +2,7 @@ using System;
 using Gtk;
 using System.IO;
 //using System.Xml.Serialization;
-using System.Data.SQLite;
-//using System.Data.SQLite.Generic;
+using Mono.Data.Sqlite;
 
 namespace SvwiktSuite
 {
@@ -12,9 +11,11 @@ namespace SvwiktSuite
 		public static void Main (string[] args)
 		{
 			Application.Init();
+			/*
 			MainWindow win = new MainWindow();
 			win.Show();
 			Application.Run();
+			*/
 
 			//kör databasläsningstest
 			ReadFromDbTest();
@@ -26,19 +27,20 @@ namespace SvwiktSuite
 		public static void ReadFromDbTest()
 		{
 			//skapa connection string
-			string folder = "..\\..\\data";
+			string folder = "../../data";
 			string connectionString = "URI=file:" + System.IO.Path.Combine (folder, "test.db");
 
 			//öppna databasen
-			SQLiteConnection db = new SQLiteConnection(connectionString);
+			var db = new SqliteConnection(connectionString);
+			//SQLiteConnection db = new SQLiteConnection(connectionString);
 			db.Open();
 
 			//SQL query
-			SQLiteCommand cmd = db.CreateCommand();
+			SqliteCommand cmd = db.CreateCommand();
 			cmd.CommandText = "SELECT pagename, tmpl_syntax FROM main";
 
 			//läs de första 100 raderna från databasen och skriv ut dem i konsollen
-			SQLiteDataReader reader =  cmd.ExecuteReader();
+			SqliteDataReader reader = cmd.ExecuteReader();
 			int count = 0;
 			while (reader.Read() && count < 100) {
 				string column1Data = reader.GetString(0);
